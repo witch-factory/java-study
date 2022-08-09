d72239afdd518b9a2c42cc93258e559e01ff6995
witch-factory <soakdma37@gmail.com>
2022-08-09 21:09:48 +0900

    자바 문자열, 패키지

---
+++ src/app/App.java
@@ -0,0 +1,8 @@
+package app;
+
+public class App {
+    public static void main(String[] args) {
+        int x=1, y=20;
+        System.out.println(x+y);
+    }
+}
---
+++ src/ex02/RunStatementExample.java
@@ -0,0 +1,8 @@
+package ex02;
+
+public class RunStatementExample {
+  public static void main(String[] args) {
+    String s="나는 \"자바\"를 좋아합니다.";
+    System.out.println(s);
+  }
+}
